c689cd6002cb03a3930beb5b872eaa9e111a2458
Slepko13 <slepko13@gmail.com>
2024-04-25 11:18:28 +0300

    update default expanded folders

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { MyBrowser } from "./components/my-browser";
 import { data } from "./data";
 
-const defaultExpandedFolders = ["/Folder A/Folder B/Folder C"];
+const defaultExpandedFolders = ["/SDK/Bootstrapper/Packages", "/VC/bin"];
 
 function App() {
   return <MyBrowser data={data} expandedFolders={defaultExpandedFolders} />;
